2bd832d8c8bbde40aa4eff0b5de7765ecc645698
julietvt <freshitpt@gmail.com>
2021-10-03 13:46:59 +0300

    add: calendar structure

--- assets/model/index.js
+++ assets/model/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+import Day from './Day.js';
+import Week from './Week.js';
+import Month from './Month.js';
+
+export default {
+  Day,
+  Week,
+  Month,
+};
--- assets/view_model/index.js
+++ assets/view_model/index.js
@@ -1,1 +1,3 @@
 'use strict';
+
+import model from '../model/index.js';
